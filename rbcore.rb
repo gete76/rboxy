94bd0851c8687f751dbc3040a791cee765671de6
Greg Traub <greg.traub@gmail.com>
2014-03-12 08:43:39 -0400

    more

--- rbcore.rb
+++ rbcore.rb
@@ -19,20 +19,98 @@ module Rboxy
 			end
 		end
 	end
-
-
 end
 
-class Builder 
-	def initialize
-		@keys = {} 
-		tag = Rboxy::Core.new('tag', {
-			'Array': array_handle()
-		})
-	end
+module Rboxy
+	module Binders
+		class Tag
+			def string_method val
 
-	def tag_def
-		arr = Proc.new{|c| }
-	end 
+			end
+
+			def array_method val
+
+			end
+
+			def hash_method val
+
+			end
+		end
+	end
+end
+
+
+module Rboxy
+	module Binders
+		class Base
+			attr_accessor :output, :js, :html, :css
+			def initialize 
+				@output=''
+				@html = ''
+				@js = ''
+				@css = ''
+			end
+			private
+			def run input
+				self.send(input.class.to_s.downcase+'_method', input)
+			end
+		end
+	end
+end
+
+module Rboxy
+	module Binders
+		#has class is a core interpreter
+		#its job is to actually read the next object
+		class Has<::Base
+
+			def string_method input
+	      #matching strings starting with ex ruby: commands
+	      #t = input.match(/(\A[a-zA-Z]{2,12}):\s/)
+	      #if t == nil
+	        @output<<input
+	        #just give back string
+	      #elsif lang_support.include?(t[1].to_s)
+	        
+	        #here we can intercept with another language if its in our language list
+	        #perhaps insert a language interpreter here
+	      #end
+			end
+
+			def array_method input
+				input.each{|v| @output << self.send('run',v)}
+			end
+
+			def hash_method val
+         #@current = default_object.merge(val)
+         t = make_object
+			end
+		end
+	end
+end
+
+
+class Builder 
+	attr_accessor :output
+	def initialize 
+		@input = nil
+		@output = nil
+		@binders = {
+			tag: Rboxy::Binders::Tag.new,
+			has: Rboxy::Binders::Has.new
+		} 
+    
+	end
+  
+  def build input
+  	@input = input
+  	run_method @input
+  end
+
+  private 
+  def run_method key, val
+  	#cls = val.class 
+  	@binders[key].run(val)
+  end
 end
 
